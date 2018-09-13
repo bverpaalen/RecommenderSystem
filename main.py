755f15e4826f904f21797cc37c0575aec019594f
medcs <m.barbouch@live.nl>
2018-09-13 20:48:08 +0200

    Rewrote the way calculating RMSE per item

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from collections import OrderedDict as OD
 from sklearn import linear_model
 
 filedir = "ml-1m/"
-filename = "ratings1000.dat"
+filename = "ratings10000.dat"
 ratings = []
 nfolds = 5
 
@@ -77,9 +77,8 @@ def printDic(dic):
 
 def trainAndTest(matrix, globalAvg, movieAvgs):
 
-    # allocate memory for results:
-    err_train = [np.zeros(nfolds), np.zeros(nfolds), np.zeros(nfolds), np.zeros(nfolds)]
-    err_test = [np.zeros(nfolds), np.zeros(nfolds), np.zeros(nfolds), np.zeros(nfolds)]
+    errTrain = {"GlobalAvg": np.zeros(nfolds), "ItemAvg": np.zeros(nfolds)}
+    errTest = {"GlobalAvg": np.zeros(nfolds), "ItemAvg": np.zeros(nfolds)}
 
     # to make sure you are able to repeat results, set the random seed to something:
     np.random.seed(17)
@@ -94,34 +93,61 @@ def trainAndTest(matrix, globalAvg, movieAvgs):
         train = ratings[train_sel]
         test = ratings[test_sel]
 
-        applyGlobalAvg(err_train, err_test, fold, train, test, globalAvg)
-        applyAvgPerItem(err_train, err_test, fold, train, test, movieAvgs)
+        applyGlobalAvg(errTrain, errTest, fold, train, test, globalAvg)
+        applyAvgPerItem(errTest, fold, train, test, movieAvgs)
+        print()
 
     # print the final conclusion:
-    #print("\n")
-    #print("Mean error on TRAIN: " + str(np.mean(err_train)))
-    #print("Mean error on  TEST: " + str(np.mean(err_test)))
+    print("\n")
+    print("Mean error item AVG: " + str(np.mean(errTest["ItemAvg"])))
 
-def applyGlobalAvg(err_train, err_test, fold, train, test, globalAvg):
+def applyGlobalAvg(errTrain, errTest, fold, train, test, globalAvg):
     print("The global average rating")
-    err_train[0][fold] = np.sqrt(np.mean((train[:, 2] - globalAvg) ** 2))
-    err_test[0][fold] = np.sqrt(np.mean((test[:, 2] - globalAvg) ** 2))
+    errTrain["GlobalAvg"][fold] = np.sqrt(np.mean((train[:, 2] - globalAvg) ** 2))
+    errTest["GlobalAvg"][fold] = np.sqrt(np.mean((test[:, 2] - globalAvg) ** 2))
 
     # print errors:
-    print("Fold " + str(fold) + ": RMSE_train=" + str(err_train[0][fold]) + "; RMSE_test=" + str(err_test[0][fold]))
+    print("Fold " + str(fold) + ": RMSE_train=" + str(errTrain["GlobalAvg"][fold]) + "; RMSE_test=" + str(errTest["GlobalAvg"][fold]))
 
-def applyAvgPerItem(err_train, err_test, fold, train, test, movieAvgs):
+def applyAvgPerItem(errTest, fold, train, test, movieAvgs):
     print("The average rating per item")
 
+    # get all unique movies, and then their ratings, after that link them in a dictionary,
+    # both for the training set as for the test set
     trainMovieIds = np.unique(train[:, 1])
     trainMovieRatings = np.array([list(train[train[:, 1] == i, 2]) for i in trainMovieIds])
-    som_err_train = 0
-    iterations = 0
-    for movieId in trainMovieIds:
-        for ratings in trainMovieRatings:
-            movieAvg = movieAvgs[movieId]
+    trainMovies = dict(zip(trainMovieIds, trainMovieRatings))
+
+    testMovieIds = np.unique(test[:, 1])
+    testMovieRatings = np.array([list(test[test[:, 1] == i, 2]) for i in testMovieIds])
+    testMovies = dict(zip(testMovieIds, testMovieRatings))
+
+    # to keep track of the RMSE's of all test sets
+    rmses = []
+    for id, ratings in trainMovies.items():
+        movieAvg = np.mean(ratings)
+
+        if id in testMovies:
+            testMovieRatings = testMovies[id]
+        else:
+            # if movie does not exist in test set
+            # use global avg of the movie
+            testMovieRatings = movieAvgs[id]
+
+        rmses.append(np.sqrt(np.mean((movieAvg - testMovieRatings) ** 2)))
+
+    #avg err per fold
+    errTest["ItemAvg"][fold] = np.mean(rmses)
+    '''
+    np.mean(ratings - movieAvg)
+            for i, testMovieRating in enumerate(testMovieRatings):
+                sse += movieAvg - testMovieRating
+
+
             som_err_train += np.mean(ratings - movieAvg)
             iterations += 1
+    
+
 
     avg_err_train = som_err_train / iterations
     err_train[1][fold] = np.sqrt(np.mean((avg_err_train) ** 2))
@@ -137,8 +163,9 @@ def applyAvgPerItem(err_train, err_test, fold, train, test, movieAvgs):
             iterations += 1
     avg_err_test = som_err_test / iterations
     err_test[1][fold] = np.sqrt(np.mean((avg_err_test) ** 2))
+    '''
 
     # print errors:
-    print("Fold " + str(fold) + ": RMSE_train=" + str(err_train[1][fold]) + "; RMSE_test=" + str(err_test[1][fold]))
+    print("Fold " + str(fold) + ": RMSE" + str(errTest["ItemAvg"][fold]))
 
 main(filedir+filename)
